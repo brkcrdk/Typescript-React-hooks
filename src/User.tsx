0e6d0b353c7df774c1c6807428e86c8db3588b15
Burak <brkcrdk@gmail.com>
2019-09-23 22:22:37 +0300

    styling

--- src/User.tsx
+++ src/User.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
-
+import "./userStyle.scss";
 import axios from "axios";
 //Fetch data types for api request
 import { UserTypes } from "./interfaces/UserInterface";
-interface Props {}
+interface Props { }
 
 const User: React.FC<Props> = () => {
   //Set state with types of defined types
   const [user, setUser] = useState<UserTypes[]>([]);
+  const [text, setText] = useState<string>("")
   const fetchUser = () => {
     axios
       .get("https://randomuser.me/api")
@@ -18,13 +19,32 @@ const User: React.FC<Props> = () => {
     fetchUser();
   }, []);
 
+  const handleHover = (name: string) => (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    setText(name)
+  }
+  console.log(user)
   return (
-    <div>
-      <ul>
-        {user &&
-          user.map((user, key) => <li key={key}>{user.location.state}</li>)}
-      </ul>
-    </div>
+    <div id="wrapper">
+      {user && user.map((user, key) => (
+        <div key={key}>
+          <div><img src={user.picture.large} /></div>
+          <div>
+            <span onMouseMove={handleHover(`${user.name.first}`)}>
+              <i className="far fa-address-card"></i>
+            </span>
+          </div>
+        </div>))}
+
+      {/* <div><img src="https://picsum.photos/150" /></div>
+      <div><h1>{text}</h1></div>
+      <div>
+        <span onMouseMove={handleHover("burak")}><i className="far fa-address-card"></i></span>
+        <span onMouseMove={handleHover("burak1")}><i className="far fa-address-card"></i></span>
+        <span><i className="far fa-address-card"></i></span>
+        <span><i className="far fa-address-card"></i></span>
+        <span><i className="far fa-address-card"></i></span>
+      </div> */}
+    </div >
   );
 };
 
--- src/interfaces/UserInterface.ts
+++ src/interfaces/UserInterface.ts
@@ -12,4 +12,7 @@ export interface UserTypes {
   };
   email: string;
   age: number;
+  picture: {
+    large: string;
+  };
 }
